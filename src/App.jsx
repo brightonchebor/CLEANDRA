1651051e1df1501e4a0493c257ac26a673870d45
brightonchebor <cheborbrighton805@gmail.com>
2025-08-11 14:19:39 +0300

    add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
-import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header'
-import Hero from './components/Hero'
 import Footer from './components/Footer'
+import AboutPage from './pages/AboutPage';
+import HomePage from './pages/HomePage';
+import ContactPage from './pages/ContactPage';
+import ServicesPage from './pages/ServicesPage';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <Header />
-      <Hero />
-      <Footer />
-    </>
+   <Router>
+      <div className="App">
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about-us" element={<AboutPage />} />
+            <Route path="/services" element={<ServicesPage />} />
+            <Route path="/contact" element={<ContactPage />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   )
 }
 

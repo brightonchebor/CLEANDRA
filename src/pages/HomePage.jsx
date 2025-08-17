6d6dcde5989ce82207da36e5e62f1fe1e36f9a2e
brightonchebor <cheborbrighton805@gmail.com>
2025-08-17 12:04:02 +0300

    Update homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Hero from '../components/Hero';
 import Services from '../components/Services';
+import Testimonials from '../components/Testimonials';
 
 const HomePage = () => {
   return (
     <div>
       <Hero />
       <Services />
+      <Testimonials />
     </div>
   );
 };
